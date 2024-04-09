d845f4a332bc925f02e6d9aa5af03a754e5ad769
EZEN-39 <KWJ5668@gmail.com>
2024-04-09 19:16:14 +0900

    cheerup

---
+++ js/cheerup.js
@@ -0,0 +1,36 @@
+const side_menu = document.querySelectorAll(".menu .under > li");
+side_menu.forEach(function (item, index) {
+  item.addEventListener("click", function () {
+    if (this.classList.contains("on")) {
+      this.classList.remove("on");
+    } else {
+      this.classList.add("on");
+    }
+  });
+});
+
+const go_top = document.querySelector(".go-top");
+window.addEventListener("scroll", function () {
+  //console.log(window.scrollY);
+  if (this.scrollY > 200) {
+    go_top.classList.add("show");
+  } else {
+    go_top.classList.remove("show");
+  }
+});
+
+go_top.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const text = document.querySelector()
+
+
+
+
+/*
+:hover 
+classList.add
+classList.remove
+classList.contains
+*/
